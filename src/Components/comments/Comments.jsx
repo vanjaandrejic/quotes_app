b499883cca5c888779222b8d6e02e56aa537dc6c
vanjaandrejic <vanjaandrejic@gmail.com>
2022-11-30 17:47:07 +0100

    comments conected to db

--- src/Components/comments/Comments.jsx
+++ src/Components/comments/Comments.jsx
@@ -1,14 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 import classes from "./Comments.module.css";
 import NewCommentForm from "./NewCommentForm";
+import { useParams } from "react-router-dom";
+import useHttp from "../../Hooks/use-http";
+import { getAllComments } from "../../Lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import CommentsList from "./CommentsList";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "completed" && (loadedComments || loadedComments.length > 0)) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (
+    status === "completed" &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className="centered">No comments were added yet!</p>;
+  }
 
   return (
     <section className={classes.comments}>
@@ -18,8 +57,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      {comments}
     </section>
   );
 };
