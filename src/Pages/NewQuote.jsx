06d0e98c88a5e173dfb97fba69eb86bce9f28e64
vanjaandrejic <vanjaandrejic@gmail.com>
2022-11-30 14:42:08 +0100

    query params setup

--- src/Pages/NewQuote.jsx
+++ src/Pages/NewQuote.jsx
@@ -1,8 +1,13 @@
 import QuoteForm from "../Components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
--- src/Pages/QuoteDetail.jsx
+++ src/Pages/QuoteDetail.jsx
@@ -1,4 +1,4 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "../Components/comments/Comments";
 import HighlightedQuote from "../Components/quotes/HighlightedQuote";
 
@@ -24,6 +24,14 @@ const QuoteDetail = () => {
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
